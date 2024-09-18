8291e1b2197b58f4a92bf081b49546dac00518bc
yng-nimish <yng.nimishs@icloud.com>
2024-09-18 17:25:05 -0400

    Added Debugging Logs to ProductsArraySun

--- src/Components/Pages/ProductsArraySun.js
+++ src/Components/Pages/ProductsArraySun.js
@@ -1,13 +1,6 @@
 // price id for you gotta do the work : price_1PoBHG013t2ai8cxq5RtiI4j
 
 const productsArraySun = [
-  // Test Mode
-  {
-    id: "price_1PxoiI013t2ai8cxpSKPhDJl",
-    title: "The Founder Series SUN",
-    price: 99.99,
-    pdfUrl: "",
-  },
   /*
   {
     // Live Mode
@@ -16,12 +9,31 @@ const productsArraySun = [
     price: 99.99,
     pdfUrl: "",
   },*/
+
+  // Test Mode
+  {
+    // Test Mode
+    id: "price_1PxoiI013t2ai8cxpSKPhDJl",
+    title: "The Founder Series SUN",
+    price: 99.99,
+    pdfUrl: "",
+  },
 ];
 
 function getProductSunData(id) {
   console.log("Looking for ID:", id);
+  console.log("Available products:", productsArraySun);
+  console.log(
+    "ID Length:",
+    id.length,
+    "Available Product ID Length:",
+    productsArraySun[0].id.length
+  );
 
-  let productSunData = productsArraySun.find((product) => product.id === id);
+  let productSunData = productsArraySun.find((product) => {
+    console.log("Comparing:", product.id, "with", id);
+    return product.id.localeCompare(id) === 0; //Fixes it to remove spacing and stuff
+  });
 
   if (productSunData === undefined) {
     console.log("Product SUN Data does not exist for ID: " + id);
